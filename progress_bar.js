b52ea594a17599883fc785893166aa3ef3869384
bartuer <bartuer@gmail.com>
2011-02-20 13:01:41 +0800

    gauge

--- progress_bar.js
+++ progress_bar.js
@@ -9,13 +9,11 @@ var tools = {};
   include('dev.js');
   include('lib.scroll.js');
   include('strokeText.js');
+  include('gauge.js');
   tools.progressBar = {};
 
-  tools.progressBar.canvas = uki.newClass(uki.view.Canvas, new
-
-  function () {
+  tools.progressBar.canvas = uki.newClass(uki.view.Canvas, new function () {
     var Base = uki.view.Canvas.prototype;
-
     this._createDom = function () {
       Base._createDom.call(this);
       this._dom.style.cssText += ';text-align:center;line-height:20px;white-space:no-wrap;font-size:12px;color:#333;-moz-border-radius:3px;-webkit-border-radius:3px;';
@@ -87,21 +85,6 @@ var tools = {};
         }]
       }]
     });
-    var canvas = p.find('Canvas')[0];
-    var ctx = canvas.width(800).height(600).ctx();
-    var w = canvas.width();
-    var h = canvas.height();
-    document.font_feature = check_textRenderContext(ctx);
-    set_textRenderContext(ctx);
-
-    ctx.fillStyle = 'grey';
-    ctx.fillRect(0, 0, w, h);
-
-    function drawtext(ctx) {
-      ctx.strokeText('240%,', 10, 10, 32, 150, 150, 100);
-      ctx.strokeText('hey', 180, 5, 24, 50, 100, 100);
-    }
-    drawtext(ctx);
 
 
     return p;
@@ -112,14 +95,36 @@ var tools = {};
     maindiv = document.getElementById('main');
     progressBar.attachTo(window, '400 400');
 
-    document['canvas_scroller'] = new Scroller('canvas_container', {
+    document.canvas_scroller = new Scroller('canvas_container', {
       scrollY: true,
       scrollX: true,
       height: 200,
       width: 200
     });
 
+    draw('canvas_container');
   };
+
+  function draw(container) {
+
+    var canvas_id = gauge.add(document.getElementById(container), {
+     width: 300,
+     height: 99,
+     radius: 0.5,
+     values: [43.15, 35, 9.65, 12.2],
+     colors: ['#316DC8', '#DF7510', '#64A8E5', '#00aa00']
+   });
+
+    var canvas = document.getElementById(canvas_id);
+    var ctx = canvas.ctx;
+
+    document.font_feature = check_textRenderContext(ctx);
+    set_textRenderContext(ctx);
+
+    ctx.strokeText('240%,', 10, 60, 32, 150, 150, 100);
+    ctx.strokeText('hey', 175, 60, 24, 50, 100, 100);
+
+  }
 
 
 })();
