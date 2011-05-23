44c56b352b9f5057174a205cec036dc14e2c37a3
bartuer <bartuer@gmail.com>
2011-05-23 11:27:30 +0800

    data change

--- data.js
+++ data.js
@@ -32,33 +32,33 @@ var progress_bar_data = {
       end: null
     }]
   },
-  weeks: [{
-    days: ['2011-02-27 Sun', '2011-02-28 Mon', '2011-03-01 Tue', '2011-03-02 Wen', '2011-03-03 Thu', '2011-03-04 Fri', '2011-03-05 Sat'],
-    values_v: [0.13, 0.8, null, 0.75, null, 1.0],
-    tasks_v: [3, 1, 2, 5, 4, 3]
-  }],
-  months: [{
-    name: 'Feb',
-    weeks: [1],
-    tasks_v: [18],
-    values_v: [0.06]
+  weeks: {
+    '8': {
+      days: [ '2011-02-28 Mon', '2011-03-01 Tue', '2011-03-02 Wed', '2011-03-03 Thu', '2011-03-04 Fri', '2011-03-05 Sat', '2011-03-06 Sun'],
+      values_v: [0.13, 0.8, null, 0.75, null, 1.0, null],
+      tasks_v: [3, 1, 2, 5, 4, 3, 0]
+    },
+    '9': {
+      days: ['2011-03-07 Mon', '2011-03-08 Tue', '2011-03-09 Wed', '2011-03-10 Thu', '2011-03-11 Fir', '2011-03-08 Sat', '2011-03-12 Sun'],
+      values_v: [0.2, 0.3, 0.72, null, null, 1.0, null],
+      tasks_v : [2, 2, 4, 5, 3, 1, 2]
+    }
   },
-  {
-    name: 'March',
-    weeks: [2, 3, 4, 5],
-    tasks_v: [21, 13, 17, 19],
-    values_v: [0.13, 0.17, 0.23, 0.29]
-  },
-  {
-    name: 'Apr',
-    weeks: [6, 7, 8, 9],
-    tasks_v: [5, 12, 11, 0],
-    values_v: null
-  },
-  {
-    name: 'May',
-    weeks: [10, 11],
-    tasks_v: [6, 8],
-    values_v: null
-  }]
+  months: {
+    'Feb': {
+      weeks: ['8'],
+      tasks_v: [18],
+      values_v: [0.06]
+    },
+    'March': {
+      weeks: ['9', '10', '11', '12'],
+      tasks_v: [21, 13, 17, 19],
+      values_v: [0.13, 0.17, 0.23, 0.29]
+    },
+    'Apr': {
+      weeks: ['13', '14', '15', '16', '17'],
+      tasks_v: [5, 12, 11, 0, 0],
+      values_v: null
+    }
+  }
 };
